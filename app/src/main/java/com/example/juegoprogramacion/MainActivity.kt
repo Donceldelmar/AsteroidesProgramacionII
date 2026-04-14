5b6de0d21fecef1bc8dc578938662ff39aa7dd6a
Donceldelmar <bell.aile.noire@gmail.com>
2026-04-14 00:10:12 -0600

    Proyecto Juego Asteroides Segunda revision

--- app/src/main/java/com/example/juegoprogramacion/MainActivity.kt
+++ app/src/main/java/com/example/juegoprogramacion/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.juegoprogramacion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,11 +14,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.juegoprogramacion.ui.theme.JuegoProgramacionTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnJugar = findViewById<Button>(R.id.Button01)
+
+        btnJugar.setOnClickListener {
+            val intent = Intent(this, JuegoActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
